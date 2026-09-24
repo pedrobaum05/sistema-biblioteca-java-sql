77cbf3405fff91511cdd6f65baffa3697eecca5d
Pedrobaum05 <pedrobaum05@gmail.com>
2026-09-24 16:46:56 -0300

    feat: cria classe Biblioteca

---
+++ src/biblioteca/Biblioteca.java
@@ -0,0 +1,10 @@
+package biblioteca;
+
+public class Biblioteca {
+    ItemBiblioteca[] itens = new ItemBiblioteca[10];
+    Usuario[] usuario = new Usuario[10];
+
+    public Biblioteca() {
+
+    }
+}
